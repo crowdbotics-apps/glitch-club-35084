17880735f6e75091d3554b8c0c939eccffec5f86
Crowdbotics <team@crowdbotics.com>
2022-05-04 21:50:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "glitch_club_35084",
+  url: "https://glitch_club_35084.botics.co",
+  api: "https://glitch_club_35084.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
